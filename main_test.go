ac50646e1e4b4408fd531c4202c6c8069152bd9e
wanuch <wanuch.int@gmail.com>
2022-10-17 23:25:04 +0700

    first commit

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+type testType struct {
+	in   string
+	want string
+}
+
+func TestLongestPalindrome(t *testing.T) {
+	tests := [...]testType{
+		{
+			in:   "babad",
+			want: "bab",
+		},
+		{
+			in:   "cbbd",
+			want: "bb",
+		},
+		{
+			in:   "",
+			want: "",
+		},
+		{
+			in:   "a",
+			want: "a",
+		},
+		{
+			in:   "abbba",
+			want: "abbba",
+		},
+	}
+	for _, tt := range tests {
+		got := longestPalindrome(tt.in)
+		if got != tt.want {
+			t.Fatalf("in: %v, got: %v, want: %v", tt.in, got, tt.want)
+		}
+	}
+}
